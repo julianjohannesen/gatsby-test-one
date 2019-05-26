4855d83e19a3e086362da92c6b45625ac3d7a5ee
julianjohannesen <github email address>
2019-05-25 20:56:38 -0400

    working on images

--- src/components/head.js
+++ src/components/head.js
@@ -1,38 +1,39 @@
 import React from 'react'
 import Helmet from 'react-helmet'
+import icon from '../images/icon.png'
 
 export default function Head() {
     return (
-        <div>
-            <Helmet
-                encodeSpecialCharacters={true}
-                titleTemplate="%s | Julian Johannesen"
-                defaultTitle="Julian Johannesen"
-                onChangeClientState={(newState, addedTags, removedTags) => console.log(newState, addedTags, removedTags)}
-            >
-                {/* Enable Google's AMP standard */}
-                <html lang="en" amp />
-                
-                {/* Base and canonical URLs 
-                <base target="_blank" href="https://awesome-northcutt-7e274f.netlify.com/" />
-                <link rel="canonical" href="https://awesome-northcutt-7e274f.netlify.com/" />
-                
-                {/* Site info */}
-                <meta name="description" content="Julian Johannesen is a front-end web developer living and working in the Boston area." />
-                <meta name="keywords" cpntent="portfolio,blog,personal,resume,professional" />
+        <Helmet encodeSpecialCharacters={true} titleTemplate="%s | Julian Johannesen" defaultTitle="Julian Johannesen" >
 
-                <meta property="og:site_name" content="Julian Johannesen" />
-                <meta property="og:type" content="website" />
-                <meta property="og:url" content="https://julianjohannesen.com" />
-                <meta property="og:locale" content="en" />
+            <html lang="en" amp />
+            
+            <base target="_blank" href="/" />
+            <link rel="canonical" href="/" />
+            
+            <meta name="description" content="Julian Johannesen is a front-end web developer living and working in the Boston area." />
+            <meta name="keywords" cpntent="portfolio,blog,personal,resume,professional" />
+            
+            <meta itemprop="name" content="Julian Johannesen" />
+            <meta itemprop="description" content="Julian Johannesen is a front-end web developer living and working in the Boston area." />
+            <meta itemprop="image" content={icon} />
+            
+            <meta property="og:site_name" content="Julian Johannesen" />
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content="https://julianjohannesen.com" />
+            <meta property="og:locale" content="en" />
+            <meta property="og:title" content="Julian Johannesen" />
+            <meta property="og:description" content="Julian Johannesen is a front-end web developer living and working in the Boston area." />
+            <meta property="og:image" content={icon} />
+            <meta property="og:image:secure_url" content={icon} />
+            <meta property="og:image:width" content="512" />
+            <meta property="og:image:height" content="512" />
+            
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:title" content="Julian Johannesen" />
+            <meta name="twitter:description" content="Julian Johannesen is a front-end web developer living and working in the Boston area." />
+            <meta name="twitter:image" content={icon} />
 
-                <meta property="og:title" content="Julian Johannesen" />
-                <meta property="og:description" content="Julian Johannesen is a front-end web developer living and working in the Boston area.." />
-                <meta property="og:image" content="" />
-                <meta property="og:image:secure_url" content="" />
-                <meta property="og:image:width" content="512" />
-                <meta property="og:image:height" content="512" />
-            </Helmet>
-        </div>
+        </Helmet>
     )
 }
